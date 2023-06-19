8106d36aee75385afdf58451bd3081d48aa09578
OleksandrNovyk <novykav@gmail.com>
2023-06-19 08:44:58 +0300

    1

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,17 @@
-import { RotatingTriangles } from 'react-loader-spinner';
+import { Dna } from 'react-loader-spinner';
 
 import { LoaderOverlay } from './Loader.styled';
 
 export const Loader = () => {
   return (
     <LoaderOverlay>
-      <RotatingTriangles
+      <Dna
         visible={true}
-        height="180"
-        width="180"
-        ariaLabel="rotating-triangels-loading"
+        height="200"
+        width="200"
+        ariaLabel="dna-loading"
         wrapperStyle={{ position: 'absolute', top: '30%' }}
-        wrapperClass="rotating-triangels-wrapper"
+        wrapperClass="dna-wrapper"
         colors={['#c47436', '#733c14', '#f1e46d']}
       />
     </LoaderOverlay>
